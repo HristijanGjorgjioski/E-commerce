def954c77c2bddaf3e411c84f623ae42f6152ae1
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-11 13:33:02 -0400

    Creating Header

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import useStyles from './styles'
+
+const Header = () => {
+    const classes = useStyles()
+
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+
+export default Header
