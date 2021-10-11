4a37b269d0cda070769d2e68f525c4e36844113e
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-11 13:44:12 -0400

    Styling the header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import useStyles from './styles'
 
 const Header = () => {
-    const classes = useStyles()
+  const classes = useStyles()
 
-    return (
-        <div>
-            Header
-        </div>
-    )
+  return (
+      <>
+        <AppBar position={fixed} className={classes.appBar} color="inherit">
+          <Toolbar>
+            <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+              
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </>
+  )
 }
 
 export default Header
---
+++ client/src/components/Header/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles, fade } from '@material-ui/core'
+
+const drawerWidth = 0
+
+export default makeStyles((theme) => ({
+    appBar: {
+        boxShadow: 'none',
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px`,
+            marginLeft: drawerWidth
+        },
+    },
+    title: {
+        flexGrow: 1,
+        alignItems: 'center'
+        display: 'flex',
+        textDecoration: 'none'
+    }
+}))
