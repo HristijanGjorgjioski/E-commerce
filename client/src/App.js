a4206b34973e5cc4f46cdb19b342c42b3e50ca66
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-12 01:56:23 -0400

    Working on Login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
+import { CssBaseline } from '@material-ui/core'
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Header from './components/Header/Header'
+import Login from './components/Login/Login'
 
 const App = () => {
   return (
     <Router>
-      <div>
+      <div style={{ display: 'flex' }}>
+        <CssBaseline />
         <Header />
+        <Switch>
+          <Route exact="/login">
+            <Login />
+          </Route>
+        </Switch>
       </div>
     </Router>
   )
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Paper, TextField, Typography } from '@material-ui/core'
 
 const Login = () => {
+    const [loginData, setLoginData] = useState({ username: '', password: '' })
+
+    const handleSubmit = () => {};
+
     return (
-        <div>
-            
-        </div>
+        <Paper elevation={6}>
+            <form autoComplete="off" onSubmit={handleSubmit}>
+                <Typography variant="h6">Login</Typography>
+                <TextField name="username" variant="outlined" label="Username" fullWidth value={loginData.username} onChange={(e) => setLoginData({ ...loginData, username: e.target.value })} />
+                <TextField name="password" variant="outlined" label="Password" fullWidth value={loginData.password} onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} />
+                <Button className={''} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+            </form>
+        </Paper>
     )
 }
 
