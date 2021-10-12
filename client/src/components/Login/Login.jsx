a01b5b8ff6c44dfde310b040817afeedd9efdcff
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-12 02:15:01 -0400

    Started to work on backend, creating folder structure

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     return (
         <Paper className={classes.paper} elevation={6}>
             <form autoComplete="off" className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Login</Typography>
+                <Typography variant="h6">Admin Login</Typography>
                 <TextField name="username" variant="outlined" label="Username" fullWidth value={loginData.username} onChange={(e) => setLoginData({ ...loginData, username: e.target.value })} />
                 <TextField name="password" variant="outlined" label="Password" fullWidth value={loginData.password} onChange={(e) => setLoginData({ ...loginData, password: e.target.value })} />
                 <Button className={''} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
