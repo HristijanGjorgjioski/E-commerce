bb6b74356956824e2d4fb8acd2eb1a5925ccf5b4
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-12 03:26:24 -0400

    Creating actions and getting ready to work with redux

---
+++ client/src/actions/auth.js
@@ -0,0 +1,10 @@
+import { AUTH } from '../constants/actionTypes'
+import * as api from '../api/index.js'
+
+export const login = (formData, router) => async (dispatch) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
