666c0de32bb2eb2fe4a0504e8283e0dd0b1e318d
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-13 02:36:57 -0400

    Working on logout

--- client/src/components/AdminMenu/AdminMenu.jsx
+++ client/src/components/AdminMenu/AdminMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Paper, Typography } from '@material-ui/core'
 
 import useStyles from './styles'
-import { ListAlt, PersonAdd, PostAdd } from '@material-ui/icons'
+import { ExitToApp, ListAlt, PersonAdd, PostAdd } from '@material-ui/icons'
 import { Link } from 'react-router-dom'
 
 const AdminMenu = () => {
@@ -14,6 +14,7 @@ const AdminMenu = () => {
                 <Typography variant="h6"><PersonAdd /><Link to='/addadmin' className={classes.linkText}> Add Admin</Link></Typography>
                 <Typography variant="h6"><ListAlt /><Link to='/allproducts' className={classes.linkText}> List All Products</Link></Typography>
                 <Typography variant="h6"><PostAdd /><Link to='/addproduct' className={classes.linkText}> Add Product</Link></Typography>
+                <Typography variant="h6"><ExitToApp /><Link to='/' className={classes.linkText}> Logout</Link></Typography>
             </div>
         </Paper>
     )
