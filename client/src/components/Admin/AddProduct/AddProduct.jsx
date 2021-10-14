fba9861aa8fea69c8a6396b9252425135bdf92e0
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-14 02:26:13 -0400

    Added createdBy option to AddProduct form

--- client/src/components/Admin/AddProduct/AddProduct.jsx
+++ client/src/components/Admin/AddProduct/AddProduct.jsx
@@ -6,7 +6,7 @@ import useStyles from './styles'
 const data = [{ name: 'Hats' }, { name: 'Female Shoes' }]
 
 const AddProduct = () => {
-    const [productData, setProductData] = useState({ title: '', description: '', tags: [], selection: '', selectedFile: '', price: '', size: '' })
+    const [productData, setProductData] = useState({ title: '', description: '', tags: [], selection: '', selectedFile: '', price: '', size: '', createdBy: {} })
     const classes = useStyles()
     const user = JSON.parse(localStorage.getItem('profile'))
 
@@ -18,14 +18,16 @@ const AddProduct = () => {
         <Paper className={classes.paper} elevation={6}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit} fullWidth>
                 <Typography variant="h6">Add Product</Typography>
-                <TextField name="title" variant="outlined" label="Title" fullWidth value={productData.title} onChange={(e) => setProductData({ ...productData, title: e.target.value })} />
-                <TextField name="description" variant="outlined" label="Description" fullWidth multiline rows={4} value={productData.description} onChange={(e) => setProductData({ ...productData, description: e.target.value })} />
-                <FormControl style={{ width: '40%' }}>
-                    <InputLabel id="size-label">Size</InputLabel>
+                <TextField disabled name="createdBy" variant="outlined" fullWidth value={user?.result?.username} onChange={(e) => setProductData({ ...productData, createdBy: e.target.value })} />
+                <TextField required name="title" variant="outlined" label="Title" fullWidth value={productData.title} onChange={(e) => setProductData({ ...productData, title: e.target.value })} />
+                <TextField required name="description" variant="outlined" label="Description" fullWidth multiline rows={4} value={productData.description} onChange={(e) => setProductData({ ...productData, description: e.target.value })} />
+                <TextField required name="price" type="number" variant="outlined" label="Price $" fullWidth value={productData.price} onChange={(e) => setProductData({ ...productData, price: e.target.value })} />
+                <FormControl required style={{ width: '40%' }}>
+                    <InputLabel id="collection-label">Collection</InputLabel>
                     <Select
-                        labelId="size-label"
-                        id="size-label-id"
-                        label="size"
+                        labelId="collection-label"
+                        id="collection-label-id"
+                        label="Collection"
                     >
                         {data.map((d) => <MenuItem key={d.name} value={d.name} onChange={(e) => setProductData({ ...productData, selection: e.target.value })}>{d.name}</MenuItem>)}
                     </Select>
