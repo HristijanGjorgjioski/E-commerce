fa61fd5a336d27ae5537c5bcf4f5239e909985b2
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-14 02:15:30 -0400

    Select option for collection is done

--- client/src/components/Admin/AddProduct/AddProduct.jsx
+++ client/src/components/Admin/AddProduct/AddProduct.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import { Paper, Select, TextField, Typography } from '@material-ui/core'
+import { FormControl, InputLabel, MenuItem, Paper, Select, TextField, Typography } from '@material-ui/core'
 
 import useStyles from './styles'
 
-const { Option } = Select
+const data = [{ name: 'Hats' }, { name: 'Female Shoes' }]
 
 const AddProduct = () => {
     const [productData, setProductData] = useState({ title: '', description: '', tags: [], selection: '', selectedFile: '', price: '', size: '' })
@@ -16,17 +16,20 @@ const AddProduct = () => {
 
     return (
         <Paper className={classes.paper} elevation={6}>
-            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit} fullWidth>
                 <Typography variant="h6">Add Product</Typography>
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={productData.title} onChange={(e) => setProductData({ ...productData, title: e.target.value })} />
                 <TextField name="description" variant="outlined" label="Description" fullWidth multiline rows={4} value={productData.description} onChange={(e) => setProductData({ ...productData, description: e.target.value })} />
-                <Select
-                    showSearch
-                    className="select-collection"
-                    placeholder="Select a Collection"
-                >
-                    <Option>Collection</Option>
-                </Select>
+                <FormControl style={{ width: '40%' }}>
+                    <InputLabel id="size-label">Size</InputLabel>
+                    <Select
+                        labelId="size-label"
+                        id="size-label-id"
+                        label="size"
+                    >
+                        {data.map((d) => <MenuItem key={d.name} value={d.name} onChange={(e) => setProductData({ ...productData, selection: e.target.value })}>{d.name}</MenuItem>)}
+                    </Select>
+                </FormControl>
             </form>
         </Paper>
     )
