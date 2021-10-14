131a9ae2c762b7646ceeea9f7cce0d3474c34987
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-14 12:20:30 -0400

    working on frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:3260/' })
+const API = axios.create({ baseURL: 'http://localhost:2260/' })
 
 export const logIn = (formData) => API.post('/user/login', formData)
 export const createAdmin = (formData) => API.post('/user/addAdmin', formData)
+
+export const createProduct = (formData) => API.post('/product/addproduct', formData)
--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,2 +1,4 @@
 export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT';
+
+export const CREATE_PRODUCT = 'CREATE_PRODUCT';
