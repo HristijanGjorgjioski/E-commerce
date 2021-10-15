78a72f226f726951c7859e3fad30f26a203d98a9
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-15 03:04:40 -0400

    Working on frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,14 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:8000/' });
 
+// AUTH APIs
 export const logIn = (formData) => API.post('/user/login', formData);
 export const createAdmin = (formData) => API.post('/user/addAdmin', formData);
 
+// PRODUCT APIs
+
+    // GET APIS
+export const getProducts = () => API.get('/get-all-products')
+
+    // POST APIs
 export const createProduct = (product) => API.post('/product/create-product', product);
