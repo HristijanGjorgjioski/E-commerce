abde1fff41b834e386431da3d6c931d734b7cc0a
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-16 11:24:08 -0400

    Product component is almost done

--- client/src/components/Admin/ListAllProducts/Product/Product.jsx
+++ client/src/components/Admin/ListAllProducts/Product/Product.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
-import { Card, ButtonBase, CardMedia, Typography, Button, CardContent } from '@material-ui/core'
-// import { MoreHoriz } from '@material-ui/icons'
+import { Card, CardMedia, Typography, CardActions, CardContent, IconButton } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
 
 import useStyles from './styles'
 
 const Product = ({ product }) => {
-    console.log(product)
+    const user = JSON.parse(localStorage.getItem('profile'))
     const classes = useStyles()
+
+    const handleAddToCart = () => {}
     
     return (
-        <Card className={classes.root} raised elevation={6}>
+        <Card className={classes.root}>
             <CardMedia className={classes.media} image={product.imageUrl || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={product.title} />
             <CardContent>
-                
+                <div className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {product.name}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        ${product.price}
+                    </Typography>
+                </div>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
             </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
         </Card>
     )
 }
