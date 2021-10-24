2e37690f4c73f4635d943c1e4981c6edf6d07269
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-24 09:42:14 -0400

    creating reducer for cart

--- client/src/actions/cart.js
+++ client/src/actions/cart.js
@@ -1,4 +1,19 @@
 import {  } from '../constants/actionTypes'
-import * as api from '../api/index.js'
 
+export const addToCart = (itemID) => {
+    return {
+      type: ADD_TO_CART,
+      payload: {
+        id: itemID,
+      },
+    };
+};
 
+export const removeFromCart = (itemID) => {
+    return {
+        type: REMOVE_FROM_CART,
+        payload: {
+            id: itemID,
+        },
+    };
+};
--- client/src/reducers/cart.js
+++ client/src/reducers/cart.js
@@ -1,3 +1,10 @@
-import {  } from '../constants/actionTypes'
+import { ADD_TO_CART } from '../constants/actionTypes'
 
-
+const cartReducer = (state = { products: [], cart: [], currentItem: null }, action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+    
+        default:
+            return state;
+    }
+}
