7b4d231f02f87ae312ad1107f0f13773c56dbcbb
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-25 03:10:57 -0400

    Having some styling errors

--- client/src/components/User/Cart/Cart.jsx
+++ client/src/components/User/Cart/Cart.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { Button, Container, Grid, Typography } from '@material-ui/core';
 
 import CartItem from './CartItem/CartItem'
+import useStyles from './styles'
 
 const Cart = () => {
-    return (
-        <div>
-            <CartItem />
-        </div>
-    )
+    const classes = useStyles()
+    const { cart } = useSelector((state) => state.cartReducer)
+
+    const renderEmptyCart = () => (
+        <Typography variant="subtitle1">You have no items in your shopping cart,
+          <Link className={classes.link} to="/">start adding some</Link>!
+        </Typography>
+      );
+    
+      if (!cart.length) return 'Loading';
+    
+      const renderCart = () => (
+        <>
+          <Grid container spacing={3}>
+            {cart.map((item) => (
+              <Grid item xs={12} sm={4} key={item._id}>
+                <CartItem item={item} />
+              </Grid>
+            ))}
+          </Grid>
+          <div className={classes.cardDetails}>
+            <Typography variant="h4">Subtotal: {cart.price}$</Typography>
+            <div>
+              <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty cart</Button>
+              <Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" color="primary">Checkout</Button>
+            </div>
+          </div>
+        </>
+      );
+    
+      return (
+        <Container>
+          <div className={classes.toolbar} />
+          <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
+          { !cart.length ? renderEmptyCart() : renderCart() }
+        </Container>
+      );
 }
 
 export default Cart
--- client/src/components/User/Cart/CartItem/CartItem.jsx
+++ client/src/components/User/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Card, CardContent, Typography, CardActions, Button } from '@material-ui/core'
+import { Card, CardContent, Typography, CardActions, Button, CardMedia } from '@material-ui/core'
 import { RemoveShoppingCart, AddShoppingCart } from '@material-ui/icons'
 
 import { addToCart, removeFromCart } from '../../../../actions/cart'
@@ -8,23 +8,24 @@ import useStyles from './styles'
 
 const CartItem = () => {
     const classes = useStyles()
-
     const { cart } = useSelector((state) => state.cartReducer)
 
     const cartItems = cart.map((item) => {
         return (
-            <Card key={item._id} className={classes.root} variant="outlined">
-                <CardContent>
-                    <Typography variant="h5" component="h2">{item.title}</Typography>
-                    <Typography color="textSecondary">{item.selection}</Typography>
-                    <Typography color="textSecondary">Size: {item.size}</Typography>
-                    <Typography color="textSecondary">Price: {item.price}</Typography>
-                    <Typography variant="body2" component="p">{item.qty}</Typography>
+            <Card className="cart-item">
+                <CardMedia image={item.imageUrl} alt={item.title} className={classes.media} />
+                <CardContent className={classes.cardContent}>
+                    <Typography variant="h4">{item.title}</Typography>
+                    <Typography variant="h5">${item.price}</Typography>
                 </CardContent>
-                <CardActions>
-                    <Button size="small" onClick={() => addToCart(item)}><AddShoppingCart color="primary" /></Button>
-                    <Button size="small" onClick={() => removeFromCart(item._id)}><RemoveShoppingCart color="secondary" /></Button>
-                </CardActions>   
+                <CardActions className={classes.cardActions}>
+                    <div className={classes.buttons}>
+                    <Button type="button" size="small">-</Button>
+                    <Typography>&nbsp;{item.qty}&nbsp;</Typography>
+                    <Button type="button" size="small">+</Button>
+                    </div>
+                    <Button variant="contained" type="button" color="secondary">Remove</Button>
+                </CardActions>
             </Card>
         );
     });
