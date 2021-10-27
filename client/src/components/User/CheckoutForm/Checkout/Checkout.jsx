2e004532c33925b8cdf64c59626801f8816b9db7
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-27 03:50:05 -0400

    Successfuly implemented stripe

--- client/src/components/User/CheckoutForm/Checkout/Checkout.jsx
+++ client/src/components/User/CheckoutForm/Checkout/Checkout.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import StripeCheckout from 'react-stripe-checkout'
+import { toast } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 
+toast.configure()
 const stripeKey = 'pk_test_51Jp641H0InCzdo9oA9wVFxxUJHr00z3c5qLc2rvDGCiAZy4qMXDpZP3TN8EiHmyQ0b7kBCXepDKrFooKDF4pKbni00ZeSC1PW2'
-
-const handleToken = (token, addresses) => {
-    console.log({ token, addresses })
-}
 
 const Checkout = () => {
     const { totalPrice } = useSelector((state) => state.cartReducer)
     console.log(totalPrice)
+
+    const handleToken = (token, addresses) => {
+        console.log({ token, addresses })
+        toast.success('Success! Your order will come to you soon!')
+    }
 
     return (
         <div>
@@ -19,7 +23,7 @@ const Checkout = () => {
                 token={handleToken}
                 billingAddress
                 shippingAddress
-                amount={totalPrice}
+                amount={totalPrice * 100}
             />
         </div>
     )
