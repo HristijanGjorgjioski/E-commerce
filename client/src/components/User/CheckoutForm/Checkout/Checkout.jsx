f90496b308242805f2e262bcc9cbadaf340fd3ad
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-27 03:53:01 -0400

    Fixed small bug with totalPrice

--- client/src/components/User/CheckoutForm/Checkout/Checkout.jsx
+++ client/src/components/User/CheckoutForm/Checkout/Checkout.jsx
@@ -8,8 +8,8 @@ toast.configure()
 const stripeKey = 'pk_test_51Jp641H0InCzdo9oA9wVFxxUJHr00z3c5qLc2rvDGCiAZy4qMXDpZP3TN8EiHmyQ0b7kBCXepDKrFooKDF4pKbni00ZeSC1PW2'
 
 const Checkout = () => {
-    const { totalPrice } = useSelector((state) => state.cartReducer)
-    console.log(totalPrice)
+    const { cart } = useSelector((state) => state.cartReducer)
+    const totalPrice = cart.reduce((acc, curr) => acc + curr.price*curr.qty, 0);
 
     const handleToken = (token, addresses) => {
         console.log({ token, addresses })
