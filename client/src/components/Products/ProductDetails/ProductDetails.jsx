1393c04e96b32d1ca8285f871742a7da9e1c4e04
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-28 03:09:44 -0400

    Impelemting logic behind ProductDetails

--- client/src/components/Products/ProductDetails/ProductDetails.jsx
+++ client/src/components/Products/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getProduct } from '../../../actions/product';
 
 const ProductDetails = () => {
+    const { product, products, isLoading } = useSelector((state) => state.productReducer);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProduct(id));
+      }, [id]);
+
     return (
         <div>
             ProductDetails
