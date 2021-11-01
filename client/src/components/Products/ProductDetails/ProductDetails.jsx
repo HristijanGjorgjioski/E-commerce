835f7426afa3c78e92312dbdb7aab833f8944efa
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-01 03:10:59 -0400

    Having progress with product detail page

--- client/src/components/Products/ProductDetails/ProductDetails.jsx
+++ client/src/components/Products/ProductDetails/ProductDetails.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { CardMedia, Container, Paper, Typography } from '@material-ui/core';
+import { Button, Checkbox, FormControl, FormControlLabel, Paper, Typography } from '@material-ui/core';
 
 import { getProduct } from '../../../actions/product';
 import useStyles from './styles'
+import { addToCart } from '../../../actions/cart';
 
 const ProductDetails = () => {
     const { product, products, isLoading } = useSelector((state) => state.productReducer);
@@ -17,23 +18,37 @@ const ProductDetails = () => {
     }, [id]);
     
     return (
-        <Paper style={{ padding: '20px', borderRadius: '15px' }} elevation={6}>
+        <Paper className={classes.paper} elevation={6}>
             <div className={classes.imageSection}>
-                <img className={classes.media} src={product?.imageUrl} alt={product.title} />
+                <img className={classes.media} src={product?.imageUrl} alt={product?.title} />
             </div>
             <div className={classes.section}>
-            <Typography gutterBottom variant="h5" component="h2">
-                        {product.title}
-                    </Typography>
-                    <Typography variant="h6" component="h2">
-                        ${product.price}
-                    </Typography>
-                    <Typography variant="body1" component="h5">
-                        Gender: {product.gender}
-                    </Typography>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {product?.title}
+                </Typography>
+                <Typography variant="body1" component="h5">
+                    Gender: {product?.gender}
+                </Typography>
+                <Typography variant="body1" component="h2">
+                    Collection: {product?.selection}
+                </Typography>
                     <Typography variant="body1" component="h2">
-                        Collection: {product.selection}
+                        Available sizes:
                     </Typography>
+                <FormControl>
+                    {product?.size?.map((s) =>
+                            <FormControlLabel
+                                style={{ flex: '1 0 21%' }}
+                                key={s}
+                                label={s}
+                                value={s}
+                                control={<Checkbox />}
+                            />
+                        )}
+                </FormControl>
+                <Button className={classes.btnAddToCart} variant="outlined" color="inherit" onClick={() => dispatch(addToCart(product))}>
+                    Add to cart - ${product?.price}
+                </Button>
             </div>
         </Paper>
     )
